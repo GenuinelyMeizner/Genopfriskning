15b589960d56e00329d6cdd386987483f43528ae
GenuinelyMeizner <frederikmpetersen@gmail.com>
2020-09-03 11:55:57 +0200

    ProperCase assignment is continued

--- src/ProperCase/Sorting.java
+++ src/ProperCase/Sorting.java
@@ -6,8 +6,17 @@ public class Sorting {
 
         if (!phrase.equals(phrase.toLowerCase())) {
 
-            System.out.println("Phrase is upper case");
+            System.out.println(phrase);
+        }
 
+        if (phrase.length() > 3) {
+            String output = phrase.substring(0, 1).toUpperCase() + phrase.substring(1);
+            System.out.println(output);
+        }
+
+        if (phrase.length() <= 3) {
+            String output2 = phrase.toLowerCase();
+            System.out.println(output2);
         }
     }
 }
